63e992f7217ceced06b143a918994a616f84d598
Azaba Daudiya <azabadaudiya13@gmail.com>
2024-02-16 10:03:58 +0530

    changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 dotenv.config({path: './config/config.env'});
 
 app.use(cors({
-    origin:[process.env.FRONTEND_URL],
+    origin:"*",
     methods:["POST"],
     credentials: true
 }));
